65ddb42b13a1844288dcf1de030320eb23d6b809
tristansoriaga <35946776+tristansoriaga@users.noreply.github.com>
2018-10-22 18:40:21 -0700

    added a message mock data preview

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,8 +1,35 @@
 import React, { Component } from "react";
 
+const DUMMY_DATA = [
+  {
+    id: 1,
+    senderId: "perborgen",
+    text: "what's going on?"
+  },
+  {
+    id: 2,
+    senderId: "jensen",
+    text: "nothing"
+  },
+  {
+    id: 3,
+    senderId: "perborgen",
+    text: "good"
+  }
+];
+
 class MessageList extends Component {
   render() {
-    return <div className="message-list">MessageList</div>;
+    return (
+      <div className="message-list">
+        {DUMMY_DATA.map(message => (
+          <div className="message" key={message.id}>
+            <p className="message-username">{message.senderId}</p>
+            <p className="message-text">{message.text}</p>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
