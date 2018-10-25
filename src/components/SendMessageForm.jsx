acbf08ad5f88b7828c96737241ca38815d881cfd
tristansoriaga <35946776+tristansoriaga@users.noreply.github.com>
2018-10-25 16:35:17 -0700

    Added a onSubmit function on sendmessageform

--- src/components/SendMessageForm.jsx
+++ src/components/SendMessageForm.jsx
@@ -1,8 +1,32 @@
 import React, { Component } from "react";
 
 class SendMessageForm extends Component {
+  state = {
+    message: ""
+  };
+
+  handleChange = e => {
+    this.setState({
+      message: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.message);
+  };
+
   render() {
-    return <form className="send-message-form">SendMessageForm</form>;
+    return (
+      <form onSubmit={this.handleSubmit} className="send-message-form">
+        <input
+          onChange={this.handleChange}
+          value={this.state.message}
+          placeholder="Type message and hit ENTER"
+          type="text"
+        />
+      </form>
+    );
   }
 }
 
